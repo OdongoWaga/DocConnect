4be84fcfe7be026d299c06d453942096e0c096e0
Waga Odongo <wagaodongo@gmail.com>
2019-02-06 17:15:18 +0300

    schemas
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router(); 
+const User = require('../../models/User');
+const gravatar = require('gravatar')
 
 //Test Users Route
 //Public
 router.get('/test', (req,res)=> res.json({
     msg:"Users Works"
 }));
+
+//@route GET api/users/register
+//Registers Users
+//Public
+router.post('/register', (req, res)=> {
+    User.findOne({
+        email: req.body.email 
+    }).then(user => {
+        if(user){
+             return res.status(400).json({email: 'Email already Exists'})
+        } else{
+            const newUser= new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                avatar
+            })
+        }
+    }) 
+});
 
 module.exports= router;
