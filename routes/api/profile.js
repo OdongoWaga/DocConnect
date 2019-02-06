296076fdd84a051f4ff48a6feef56c1246b91fc3
Waga Odongo <wagaodongo@gmail.com>
2019-02-06 16:36:43 +0300

    routes
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router(); 
+
+//Test Profile Route
+//Public
+router.get('/test', (req,res)=> res.json({
+    msg:"Profile Works"
+}));
+
+module.exports= router;
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
  const express = require('express');
  const app = express();
+ const mongoose= require('mongoose');
+
+ const users = require('./routes/api/users');
+ const profile = require('./routes/api/profile');
+ const posts = require('./routes/api/posts');
+ 
 
  app.get('/', (req, res) => res.send('hello'));
 
- const port= process.env.PORT || 5000;
+//DB Setup
+
+const db = require('./config/keys.js').mongoURI
+
+//Connect to DB
+
+mongoose
+.connect(db)
+.then(()=> console.log('MongoDB Connected'))
+.catch(err=> console.log(err)); 
+
+//Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+
+const port= process.env.PORT || 5000;
  
  app.listen( port, ()=> console.log(`Server running at ${port}`));
