dcf87b04c67c94fa21afaabd68334315ce5ed8c3
Waga Odongo <wagaodongo@gmail.com>
2019-02-07 11:25:29 +0300

    11:25
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router(); 
+const mongoose = require('mongoose');
+const passport = require('passport');
+//Load Profile
+const Profile= require('../../models/Profile');
+//Load User
+const User= require('../../models/User')
 
 //Test Profile Route
 //Public
@@ -7,7 +13,25 @@ router.get('/test', (req,res)=> res.json({
     msg:"Profile Works"
 }));
 
+// GET api/profile
+//get users profile
+// private
+router.get('/', passport.authenticate('jwt', {session:false}), (req,res)=> {
+const errors= {};
 
+Profile.findOne({user: req.user.id})
+.then(profile => {
+if(!profile){
+    errors.noprofile= 'There is no profile for the user';
+    return res.status(404).json(errors);
+}
+res.json(profile);
+})
+.catch(err => res.status(404).json(err));
+
+
+});
  
+  
 
 module.exports= router;
