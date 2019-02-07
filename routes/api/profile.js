907e53342fec93373a84ebd28013faff17162127
Waga Odongo <wagaodongo@gmail.com>
2019-02-07 15:07:34 +0300

    profile backend
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -31,7 +31,56 @@ res.json(profile);
 
 
 });
- 
+
+//Test Profile Route
+//Public
+router.get('/test', (req,res)=> res.json({
+    msg:"Profile Works"
+}));
+
+// POST api/profile
+//create /edit users profile
+// private
+router.post('/', passport.authenticate('jwt', {session:false}), 
+(req,res)=> {
+
+    //Get Fields
+
+profileFields.user = req.user.id;
+if(req.body.handle) profileFields.handle = req.body.handle;
+if(req.body.clinic) profileFields.clinic = req.body.clinic;
+if(req.body.website) profileFields.website = req.body.website;
+if(req.body.location) profileFields.location = req.body.location;
+if(req.body.bio) profileFields.bio = req.body.bio;
+
+//Skills -split into array
+if(typeof req.body.skills !== 'undefined'){
+    profileFields.skills= req.body.skills.split(',');
+}
+
+profileFields.social = {};
+
+if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+Profile.findOne({user: req.user.id }).then (profile=> {
+    if(profile) {
+        //Update
+
+    Profile.findOneAndUpdate(
+        {user: req.user.id},
+        {$set:profileFields },
+        {new: true}
+    ).then(profile => res.json(profile));
+    } else {
+        //Create
+    }
+})
+
+});
+
   
 
 module.exports= router;
