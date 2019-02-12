278f61e678977df966aa3544cb301988300d4b69
Waga Odongo <wagaodongo@gmail.com>
2019-02-12 11:07:40 +0300

    jwt decoded
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
  import axios from 'axios';
- import {GET_ERRORS} from './Types'
+ import {GET_ERRORS,SET_CURRENT_USER} from './Types'
+ import setAuthToken from '../utils/setAuthToken';
+ import jwt_decode from 'jwt-decode';
 
 
  //Register User
@@ -13,9 +15,41 @@
         dispatch({
             type: GET_ERRORS,
             payload: err.response.data
-        }) );
+        }) ); 
  };
 
  //Login Get User Token
 
-  
+  export const loginUser = userData => dispatch => {
+      axios.post('/api/users/login', userData)
+      .then(res => {
+          //save to local storage
+        const {token}=res.data;
+        //set token to local storage
+        localStorage.setItem('jwtToken', token ); 
+
+        //set token to Auth header
+        setAuthToken(token);
+        // Decode token to get user data
+
+        const decoded = jwt_decode(token);
+
+        //Set Current User
+
+        dispatch(setCurrentUser(decoded));
+
+      })
+      .catch(err=> dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+      }))
+  }
+
+  //Set logged in User
+ 
+  export const setCurrentUser =(decoded) => {
+      return{
+          type: SET_CURRENT_USER,
+          payload:decoded
+      }
+  }
