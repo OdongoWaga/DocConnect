6b46c6d43d16462df72b79974af57204a4bbfcc3
Waga Odongo <wagaodongo@gmail.com>
2019-02-13 08:01:09 +0300

    profile creation
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/create profile/CreateProfile.js
+++ client/src/components/create profile/CreateProfile.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import TextFieldGroup from '../../utils/textFieldGroup';
+import SelectListGroup from '../../utils/selectListGroup';
+import TextAreaFieldGroup from '../../utils/textAreaFieldGroup';
 
 class CreateProfile extends Component {
     state ={
@@ -19,7 +22,34 @@ class CreateProfile extends Component {
         errors:{}
     }
   
+  onSubmit=(e) => {
+      e.preventDefault();
+
+      console.log('submit');
+  } 
+
+  onChange=(e)=> {
+      this.setState({[e.target.name]: e.target.value})
+  }
+    
     render() {
+        const {errors} =this.state;
+
+     //Select options for status
+     
+     const options= [
+
+    {label: 'Select Professional Status', value: 0},
+    {label: 'Student MedSchool', value: 'Student MedSchool'},
+    {label: 'Student Medical College', value: 'Student Medical College'},
+    {label: 'Medical Intern', value: 'Medical Intern'},
+    {label: 'Other', value: 'Other'},
+    {label: 'Doctor', value: 'Doctor'},
+    {label: 'Medical Specialist', value: 'Medical Specialist'},
+    {label: 'Senior Doctor', value: 'Senior Doctor'},
+
+     ];
+
     return (
       <div className="create-profile">
       <div className="container">
@@ -30,13 +60,84 @@ class CreateProfile extends Component {
       Lets get some information about you. 
       </p>
       <small className="d-block pb-3">*=required fields </small>
+      <form onSubmit={this.onSubmit}>
+      <TextFieldGroup 
+       placeholder="* Profile Handle"
+       name="handle"
+       value={this.state.handle}
+       onChange={this.onChange}
+       error={errors.handle}
+       info="A unique handle for your profile URL."
+      />
+      <SelectListGroup 
+       placeholder="Status"
+       name="status"
+       value={this.state.status}
+       onChange={this.onChange}
+        options={options}
+       error={errors.status}
+       info="Give Us an idea of where you are in your career "
+      />
+      <TextFieldGroup 
+       placeholder="* Profile Handle"
+       name="handle"
+       value={this.state.handle}
+       onChange={this.onChange}
+       error={errors.handle}
+       info="A unique handle for your profile URL."
+      />
+      <TextFieldGroup 
+       placeholder="Clinic"
+       name="clinic"
+       value={this.state.clinic}
+       onChange={this.onChange} 
+       error={errors.clinic}
+       info="Tell us more about your workplace"
+      />
+      <TextFieldGroup 
+       placeholder="website"
+       name="website"
+       value={this.state.website}
+       onChange={this.onChange} 
+       error={errors.website}
+       info="Website address"
+      />
+      <TextFieldGroup 
+       placeholder="location"
+       name="location"
+       value={this.state.location}
+       onChange={this.onChange} 
+       error={errors.location}
+       info="Where are you based"
+      />
+      <TextFieldGroup 
+       placeholder="Skills"
+       name="skills"
+       value={this.state.skills}
+       onChange={this.onChange} 
+       error={errors.skills}
+       info="use comma seperated values (eg surgery, nutrition, epidemiology)"
+      />
+      <TextAreaFieldGroup
+       placeholder="Skills"
+       name="skills"
+       value={this.state.skills}
+       onChange={this.onChange} 
+       error={errors.skills}
+       info="use comma seperated values (eg surgery, nutrition, epidemiology"
+      />
+
+        <div className="mb-3">
+        <button className="btn btn-light"> </button>
+        </div>
+      </form>
       </div>
       </div>
       
       </div>
         
       </div>
-    )
+    ) 
   }
 }
 
@@ -45,4 +146,5 @@ const mapStateToProps =state => ({
     errors: state.errors
 });
 
-export default connect(null)(CreateProfile)
+export default connect(mapStateToProps)(CreateProfile)
+ 
