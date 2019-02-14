316187b399ffe30a4a5d26929527990a5bfe82c2
Waga Odongo <wagaodongo@gmail.com>
2019-02-14 17:31:50 +0300

    profilesubcomponents
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {getProfileByHandle} from '../../actions/profileActions';
+import ProfileCreds from './ProfileCreds';
+import ProfileHeader from './ProfileHeader';
+import ProfileAbout from './ProfileAbout';
 
  class Profile extends Component {
 componentDidMount (){
@@ -13,6 +16,9 @@ if(this.props.match.params.handle){
   render() {
     return (
       <div>
+          <ProfileHeader />
+          <ProfileAbout />
+          <ProfileCreds />
         
       </div>
     )
@@ -22,4 +28,4 @@ const mapStateToProps =state => ({
 profile:state.profile
 });
 
-export default connect(mapStateToProps)(Profile)
+export default connect(mapStateToProps, {getProfileByHandle})(Profile)
